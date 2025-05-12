5918a77ae73bb2d087b0464f9efc247d5117ebeb
Giuseppe Carbone <Giuseppe687.gc@gmail.com>
2025-05-12 17:33:50 -0400

    Simple Commit
    Added modded server for dalyla and fixed some typos

--- server_configs.py
+++ server_configs.py
@@ -15,26 +15,12 @@ SERVER_CONFIGS = {
         'startup_program': './start.sh',
 	'log_file': '/home/dealbreacker/servers/bros_server_1.21.2/logs/latest.log'
     },
-    'all_of_fabric': {
+    'personal_server': {
         'host': '192.168.1.130',
         'port': 25568,
-        'startup_script': '/home/dealbreacker/servers/all_of_fabric_7_2.3.0/',
-        'startup_program': './startserver.sh',
-	'log_file': '/home/dealbreacker/servers/all_of_fabric_7_2.3.0/logs/latest.log'
-    },
-    'all_the_mods_9-THS': {
-        'host': '192.168.1.130',
-	'port': 25570,
-	'startup_script': '/home/dealbreacker/servers/all_the_mods_9-TO_THE_SKY/',
-	'startup_program': './startserver.sh',
-	'log_file': '/home/dealbreacker/servers/all_the_mods_9-TO_THE_SKY/logs/latest.log'
-    },
-    'evolution': {
-        'host': '192.168.1.130',
-    'port': 25569,
-    'startup_script': '/home/dealbreacker/servers/evolution/',
-    'startup_program': './run.sh',
-    'log_file': '/home/dealbreacker/servers/evolution/logs/latest.log'
+        'startup_script': '/home/dealbreacker/servers/bros_server_1.21.2/',
+        'startup_program': './start.sh',
+	'log_file': '/home/dealbreacker/servers/bros_server_1.21.2/logs/latest.log'
     }
 }
 #~~~~~~~~~~~~~~ This is the location for servers that are currently under maintinance/out of comission~~~~~~~~~
@@ -44,6 +30,35 @@ SERVER_CONFIGS = {
 #        'startup_script': '/home/dealbreacker/servers/creative_server/',
 #        'startup_program': './start.sh',
 #	'log_file': '/home/dealbreacker/servers/creative_server/logs/latest.log'
+
+
+# 'all_of_fabric': {
+#     'host': '192.168.1.130',
+#     'port': 25568,
+#     'startup_script': '/home/dealbreacker/servers/all_of_fabric_7_2.3.0/',
+#     'startup_program': './startserver.sh',
+# 'log_file': '/home/dealbreacker/servers/all_of_fabric_7_2.3.0/logs/latest.log'
+# },
+
+
+# 'all_the_mods_9-THS': {
+#     'host': '192.168.1.130',
+# 'port': 25570,
+# 'startup_script': '/home/dealbreacker/servers/all_the_mods_9-TO_THE_SKY/',
+# 'startup_program': './startserver.sh',
+# 'log_file': '/home/dealbreacker/servers/all_the_mods_9-TO_THE_SKY/logs/latest.log'
+# },
+
+
+# 'evolution': {
+#     'host': '192.168.1.130',
+# 'port': 25569,
+# 'startup_script': '/home/dealbreacker/servers/evolution/',
+# 'startup_program': './run.sh',
+# 'log_file': '/home/dealbreacker/servers/evolution/logs/latest.log'
+# },
+
+#~~~~~~~~~~~~~~~~~~~ End of unused servers ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     # Add more servers as needed
 
